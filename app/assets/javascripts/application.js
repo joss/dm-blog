eeb87f29a601547ca59df64a548361e3551f5f98
Petr Balaban <balaban.petr@gmail.com>
2014-05-17 00:12:23 +0400

    Install jquery.fileupload.js plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,11 @@
 //= require bootstrap
 //= require underscore
 
+//= require jquery.ui.widget
+//= require load-image.min
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require jquery.fileupload.process
+//= require jquery.fileupload.image
+
 //= require ./main
